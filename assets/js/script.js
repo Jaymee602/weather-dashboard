e8fced455455a639820c26975e03bae4233b03ef
jam <jaymee602@gmail.com>
2022-09-04 20:21:26 -0700

    append searched city to search history container

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var apiKey = "ca8f8ea9fc3dba6a7b9c869686179db1";
 var searchForm = document.querySelector('#search-form');
 var citySearch = document.querySelector('#city-search');
 var searchBtn = document.querySelector('#search-btn');
-var searchHistory = document.querySelector('#search-history');
+var searchHistoryContainer = document.querySelector('#search-history');
 var fiveDay = document.querySelector('#five-day');
 
 var todaysDate = moment().format('dddd, MMM Do YYYY');
@@ -16,21 +16,32 @@ var formSubmitHandler = function(event) {
     // get value from input element
     var city = citySearch.value.trim();
   
-    // if (city) {
+    if (city) {
     //   getWeather(city);
     //   getFiveDay(city);
+
+        // save searched city to local storage
+        var saveCity = function(city) {
+            localStorage.setItem("cities", JSON.stringify(city));
+        };
+
+        // make search history button
+        function searchHistory() {
+            cityBtn = document.createElement("button");
+            cityBtn.textContent = city;
+            cityBtn.className = "city-btn";
+            searchHistoryContainer.append(cityBtn)
+        };
   
-    //   // clear search bar
-    //   citySearch.value = "";
-    // } else {
-    //   alert("Please enter a city");
-    // }
+        // clear search bar
+        citySearch.value = "";
+
+    } else {
+      alert("Please enter a city");
+    }
     saveCity(city);
+    searchHistory(city);
 };
 
-// save searched city to local storage
-var saveCity = function(city) {
-    localStorage.setItem("cities", JSON.stringify(city));
-};
 
 searchForm.addEventListener("submit", formSubmitHandler);
